9b3f47d51c503167e604e548a6f5e76820674c42
mogobon <mogobon@icloud.com>
2025-03-28 07:21:46 +0900

    vite.configの修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: '/', // または './' に変更
+  build: {
+    outDir: 'dist',
+  },
   plugins: [solid()],
-})
+});
