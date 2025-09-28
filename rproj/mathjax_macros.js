c87e70cb04aeffec40c44f4e73527751586f10aa
mogobon <mogobon@users.noreply.github.com>
2025-09-28 13:18:17 +0000

    deploy: 3f66b70fcfca0cfa275713f9a60dbc3604bf6fce

---
+++ rproj/mathjax_macros.js
@@ -0,0 +1,16 @@
+<script>
+window.MathJax = {
+  loader: {load: ['[tex]/colorv2']},
+  tex: {
+    packages: {'[+]': ['colorv2']},
+    macros: {
+      bm: ["{\\boldsymbol{#1}}", 1]
+    },
+    inlineMath: [['$', '$'], ['\\(', '\\)']]
+  }
+};
+</script>
+
+<script id="MathJax-script" async
+  src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">
+</script>
